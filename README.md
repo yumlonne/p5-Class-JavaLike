e909adff5042277cb7ae1fbf8d43d1bc7aa2ec08
yumlonne <yumlonne@gmail.com>
2018-11-08 20:06:01 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Class::JavaLike - It's new $module
+
+# SYNOPSIS
+
+    use Class::JavaLike;
+
+# DESCRIPTION
+
+Class::JavaLike is ...
+
+# LICENSE
+
+Copyright (C) yumlonne.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+yumlonne <yumlonne@gmail.com>
---
+++ lib/Class/JavaLike.pm
@@ -0,0 +1,39 @@
+package Class::JavaLike;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Class::JavaLike - It's new $module
+
+=head1 SYNOPSIS
+
+    use Class::JavaLike;
+
+=head1 DESCRIPTION
+
+Class::JavaLike is ...
+
+=head1 LICENSE
+
+Copyright (C) yumlonne.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+yumlonne E<lt>yumlonne@gmail.comE<gt>
+
+=cut
+
